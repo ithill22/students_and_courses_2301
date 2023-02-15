2579c87569c78a4ff3f12a1e39067bc073186e2c
Isaac Thill <ithill22@turing.edu>
2023-02-15 09:30:12 -0700

    test: add test for initialize

---
+++ spec/course_spec.rb
@@ -0,0 +1,19 @@
+require './lib/course'
+require './lib/student'
+
+RSpec.describe Course do
+  before(:each) do
+    @course = Course.new("Calculus", 2) 
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})   
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@course).to be_an_instance_of(Course)
+      expect(@course.name).to eq('Calculus')
+      expect(@course.capacity).to eq(2)
+      expect(@course.students).to eq([])
+    end
+  end
+end
