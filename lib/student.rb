0229d78127e4f2847d51ba7390460c6855f6de59
Isaac Thill <ithill22@turing.edu>
2023-02-15 09:17:24 -0700

    feat: def method for log_score

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,12 @@
 class Student
-  attr_reader :name, :age
+  attr_reader :name, :age, :scores
   def initialize(name_age)
     @name = name_age[:name]
     @age = name_age[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
   end
 end
