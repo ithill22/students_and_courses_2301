07f5089ed1dd3b2b8126ecfd882f5f99433fe7c5
Isaac Thill <ithill22@turing.edu>
2023-02-15 10:01:59 -0700

    test: add test for add_course method

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -5,7 +5,9 @@ require './lib/student'
 RSpec.describe Gradebook do
   before(:each) do
     @gradebook = Gradebook.new("Mr. Stack")
-    @course = Course.new("Calculus", 2) 
+    @calculus = Course.new("Calculus", 2) 
+    @history = Course.new("History", 4)
+    @science = Course.new("Science", 3)
     @student1 = Student.new({name: "Morgan", age: 21})
     @student2 = Student.new({name: "Jordan", age: 29}) 
   end
@@ -15,6 +17,15 @@ RSpec.describe Gradebook do
       expect(@gradebook).to be_an_instance_of(Gradebook)
       expect(@gradebook.instructor).to eq('Mr. Stack')
       expect(@gradebook.courses).to eq([])
+    end
+  end
+
+  describe '#add_course' do
+    it 'can add a course to courses array' do
+      @gradebook.add_course(@calculus)
+      @gradebook.add_course(@history)
+
+      expect(@gradebook.courses).to eq([@calculus, @history])
     end
   end
 end
