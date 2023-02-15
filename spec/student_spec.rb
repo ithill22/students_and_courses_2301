a138095484752e6c2661f51c5d4c4a4c1964b8e3
Isaac Thill <ithill22@turing.edu>
2023-02-15 09:17:04 -0700

    test: add test for log_score

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe Student do
       expect(@student).to be_an_instance_of(Student)
       expect(@student.name).to eq('Morgan')
       expect(@student.age).to eq(21)
+      expect(@student.scores).to eq([])
+    end
+  end
+
+  describe '#log_score' do
+    it 'can add a score to the scores array' do
+      expect(@student.scores).to eq([])
+
+      @student.log_score(89)
+      @student.log_score(78)    
+
+      expect(@student.scores).to eq([89, 78])
     end
   end
 end
